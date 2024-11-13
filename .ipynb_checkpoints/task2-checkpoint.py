c5f9bbf70d4d322501142100264820b24056ccda
Sasha <st134267@student.spbu.ru>
2024-11-13 16:32:42 +0000

    my code

--- .ipynb_checkpoints/task2-checkpoint.py
+++ .ipynb_checkpoints/task2-checkpoint.py
@@ -1,5 +1,21 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def is_power(a,b):
+    # accounting for specific obvious cases first
+    if b == 1:
+        return a == 1 # since it will always be 1
+    
+# not going to account for non-integer or negative cases here, running out of time
+
+    # initializing the counter for powers that we're gonna be checking
+    power_count = 1 
+    b_mult = b # this var will store the result of our multiplications
+    while b_mult < a:
+        b_mult *= b
+        power_count += 1 # incrementing the counter
+    if b_mult == a:
+        print(f"True, because {a} is {b} to the {power_count}th power")
+        return True # it returns True a second time but it's okay I guess for now
+    else:
+        return False
+
+# testing the function
+print(is_power(16,2))
